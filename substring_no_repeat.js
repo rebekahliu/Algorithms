0dbc7eb9e576d6b0e05c38737663c785a27ed025
Rebekah Liu <liu.rebekah@gmail.com>
2017-11-23 15:49:37 -0800

    substring no repeats

--- substring_no_repeat.js
+++ substring_no_repeat.js
@@ -1,12 +1,14 @@
 // better algorithm
 // initialize array of 256 elements new Array(256)(-1)
-// iterate through letters, changing array to save last occurrance
+// iterate through characters, changing array to save last occurrance
+// if character already occurred, save that substring
+// start again from the letter after the former occrance of repeated letter
 
 var lengthOfLongestSubstring = function(s) {
     let letters = {};
     let longestString = "";
     let string = "";
-    
+
     for (let i = 0; i < s.length; i++){
         let letter = s[i];
         if (typeof letters[letter] != 'number') {
@@ -21,7 +23,7 @@ var lengthOfLongestSubstring = function(s) {
           string = "";
         }
     }
-    
+
     if (string.length > longestString.length) {
       longestString = string;
     }
